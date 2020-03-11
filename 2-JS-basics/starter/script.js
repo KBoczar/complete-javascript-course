4047fa7ebc3185fb450c298348b88b74aecf2595
Krzysiek Boczar <41698980+KBoczar@users.noreply.github.com>
2020-03-11 20:51:48 +0100

    test
    test1

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,11 @@
+var firstName = 'John';
+var lastName = 'Smith';
+var age = 28;
+var fullAge = true;
+if(age === 28){
+    fullAge = true;
+    
+}else fullAge = false;
+var job;
+job = 'Teacher'
+console.log(firstName, lastName, age, fullAge, job);
